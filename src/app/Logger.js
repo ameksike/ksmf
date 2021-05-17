7439313d0c87f3eff3a4d2e6c183e7e7462fb6bf
Antonio Membrides Espinosa <tonykssa@gmail.com>
2021-05-17 19:44:01 -0400

    publish v1.0.17

--- src/app/Logger.js
+++ src/app/Logger.js
@@ -9,30 +9,63 @@
 class Logger {
 
     constructor(opt = null) {
-        this.cfg = { level: 0 };
+        this.cfg = {
+            level: 0,
+            prefix: 'DEFAULT',
+            label: {
+                log: 'LOG',
+                debug: 'DEBUG',
+                error: 'ERROR',
+                info: 'INFO',
+                warn: 'WARN',
+            }
+        };
         this.configure(opt);
     }
 
     configure(opt = null) {
-        this.cfg = opt || this.cfg;
+        this.cfg = Object.assign(this.cfg, opt);
+        return this;
+    }
+
+    prefix(value) {
+        this.cfg.prefix = value;
+        return this;
     }
 
     log() {
-        if (this.cfg && this.cfg.level === 1) {
-            console.log.apply(this, arguments);
+        if (this.cfg && this.cfg.level >= 1) {
+            console.log.apply(this, [`[${this.cfg.prefix}] [${this.cfg.label.info}]`, ...arguments]);
         }
+        return this;
     }
 
-    info(){
-        if (this.cfg && this.cfg.level === 1) {
-            console.log.apply(this, arguments);
+    info() {
+        if (this.cfg && this.cfg.level >= 1) {
+            console.log.apply(this, [`[${this.cfg.prefix}] [${this.cfg.label.info}]`, ...arguments]);
         }
+        return this;
     }
 
-    error(){
-        if (this.cfg && this.cfg.level === 1) {
-            console.log.apply(this, arguments);
+    error() {
+        if (this.cfg && this.cfg.level >= 1) {
+            console.log.apply(this, [`[${this.cfg.prefix}] [${this.cfg.label.error}]`, ...arguments]);
         }
+        return this;
+    }
+
+    warn() {
+        if (this.cfg && this.cfg.level >= 1) {
+            console.log.apply(this, [`[${this.cfg.prefix}] [${this.cfg.label.warn}]`, ...arguments]);
+        }
+        return this;
+    }
+
+    debug() {
+        if (this.cfg && this.cfg.level >= 1) {
+            console.log.apply(this, [`[${this.cfg.prefix}] [${this.cfg.label.debug}]`, ...arguments]);
+        }
+        return this;
     }
 }
 
