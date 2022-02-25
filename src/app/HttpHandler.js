00bcfa823417e5a52e54871fd2dfc3aa95a7f385
Antonio Membrides Espinosa <tonykssa@gmail.com>
2022-02-25 08:31:36 -0300

    Use an Object Literal instead

--- src/app/HttpHandler.js
+++ src/app/HttpHandler.js
@@ -8,51 +8,52 @@
  * */
 class HttpHandler {
 
+    constructor() {
+        this.ation = {
+            200: (res) => {
+                res.write([
+                    'HTTP/1.1 200 Connection Established',
+                    'Proxy-agent: KSMF'
+                ].join('\r\n'));
+                res.write('\r\n\r\n');
+            },
+            400: (res) => {
+                res.write('HTTP/1.1 400 Bad Request\r\n');
+                res.end('\r\n\r\n');
+                res.destroy();
+            },
+            403: (res) => {
+                res.write('HTTP/1.1 403 Forbidden\r\n');
+                res.end('\r\n\r\n');
+            },
+            407: (res) => {
+                res.write([
+                    'HTTP/1.1 407 Proxy Authentication Required',
+                    'Proxy-Authenticate: Basic realm="proxy"',
+                    'Proxy-Connection: close',
+                ].join('\r\n'));
+                res.end('\r\n\r\n');
+            },
+            500: (res) => {
+                res.end(`HTTP/1.1 500 External Server End\r\n`);
+                res.end('\r\n\r\n');
+            },
+            custom: (res, message) => {
+                res.end(`HTTP/1.1 500 ${message}\r\n`);
+                res.end('\r\n\r\n');
+            }
+        }
+    }
+
     send(res, code, message) {
         if (!res || !code || res.finished || res.writable !== true) return false;
         try {
-            switch (code) {
-                case 200:
-                    res.write([
-                        'HTTP/1.1 200 Connection Established',
-                        'Proxy-agent: KSMF'
-                    ].join('\r\n'));
-                    res.write('\r\n\r\n');
-                    break;
-
-                case 400:
-                    res.write('HTTP/1.1 400 Bad Request\r\n');
-                    res.end('\r\n\r\n');
-                    res.destroy();
-                    break;
-
-                case 403:
-                    res.write('HTTP/1.1 403 Forbidden\r\n');
-                    res.end('\r\n\r\n');
-                    break;
-
-                case 407:
-                    res.write([
-                        'HTTP/1.1 407 Proxy Authentication Required',
-                        'Proxy-Authenticate: Basic realm="proxy"',
-                        'Proxy-Connection: close',
-                    ].join('\r\n'));
-                    res.end('\r\n\r\n');
-                    break;
-
-                case 500:
-                    res.end(`HTTP/1.1 500 External Server End\r\n`);
-                    res.end('\r\n\r\n');
-                    break;
-
-                default:
-                    res.end(`HTTP/1.1 500 ${message}\r\n`);
-                    res.end('\r\n\r\n');
-                    break;
+            if (this.ation[code]) {
+                this.ation[code](res, message);
             }
         }
         catch (error) {
-            console.log('ERROR', 'HttpHandler', error);
+            console.log('[ERROR]', 'HttpHandler', error);
         }
     }
 }
