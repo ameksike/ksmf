d8805a0b005e9cadc3acf3ec2b15bb1ba0df7d4e
Antonio Membrides Espinosa <tonykssa@gmail.com>
2024-04-28 18:24:19 +0200

    update comments

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * @author      Antonio Membrides Espinosa
  * @email       tonykssa@gmail.com
  * @date        20/05/2020
- * @copyright   Copyright (c) 2020-2035
+ * @copyright   Copyright (c) 2020-2055
  * @license     GPL
  * @version     1.0
  * @description For more information see: https://github.com/ameksike/ksmf/wiki  
