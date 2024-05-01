76d34005346f675d1d25eee5d2fe79724fafd7e4
Antonio Membrides Espinosa <tonykssa@gmail.com>
2024-05-01 17:17:39 +0200

    add the app plugin

---
+++ src/plugin/App.js
@@ -0,0 +1,15 @@
+/**
+ * @author      Antonio Membrides Espinosa
+ * @email       tonykssa@gmail.com
+ * @date        01/05/2024
+ * @copyright   Copyright (c) 2020-2050
+ * @license     GPL
+ * @version     1.4
+ **/
+const AppBase = require('../app/App');
+
+class AppModule extends AppBase {
+
+}
+
+module.exports = AppModule;
